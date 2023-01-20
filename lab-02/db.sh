b3338ec5bebdc9a7eadbcd6cac4a7b6315f53851
Erlant Farias <54916109+CheaterDoom@users.noreply.github.com>
2023-01-20 15:13:46 -0300

    commit 2

--- lab-02/db.sh
+++ lab-02/db.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y docker.io docker-compose
-sudo usermod -aG docker vagrant
 sudo systemctl enable docker
 sudo systemctl start docker
-cd /vagrant
 docker-compose up -d
 sudo docker ps
--- lab-02/wordpress.sh
+++ lab-02/wordpress.sh
@@ -4,9 +4,7 @@ ip -br a
 sudo apt update
 sudo apt install -y docker.io docker-compose
 sudo systemctl enable --now docker docker.socket containerd
-sudo usermod -aG docker vagrant
 sudo systemctl enable docker
 sudo systemctl start docker
-cd /vagrant
 docker-compose up -d
 sudo docker ps
